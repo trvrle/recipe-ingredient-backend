38e8185d67fa2dd877cc836b8fe49ca7c95fae59
Trevor Le <trevor.le@altaml.com>
2023-09-12 17:45:24 -0600

    Implement user model and add dependencies

---
+++ src/main/java/com/trevor/recipe/model/user/User.java
@@ -0,0 +1,19 @@
+package com.trevor.recipe.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+}
