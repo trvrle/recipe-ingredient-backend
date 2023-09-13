9653929645e2e317458ac4dbfda40e752ebd5d43
Trevor Le <trevor.le@altaml.com>
2023-09-13 14:50:25 -0600

    Connect backend to RDS database

--- src/main/java/com/trevor/recipe/controller/UserController.java
+++ src/main/java/com/trevor/recipe/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.trevor.recipe.controller;
 
+import com.trevor.recipe.model.user.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "user")
 public class UserController {
-
+    
     @GetMapping
-    public String getUser() {
-        return "Hello World!";
+    public User getUser() {
+        User user = new User(1, "Trevor", "Le", "trevor.le", "password");
+
+        return user;
     }
 }
--- src/main/java/com/trevor/recipe/model/user/User.java
+++ src/main/java/com/trevor/recipe/model/user/User.java
@@ -1,5 +1,8 @@
 package com.trevor.recipe.model.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,13 +10,26 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User implements Serializable {
+
+    @Id
+    @Column(name = "id")
     private int id;
-    private String name;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
 }
