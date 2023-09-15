1efc0a4cb81cab55cfb0f3240c9c765511446a58
Trevor Le <trevor.le@altaml.com>
2023-09-15 14:31:01 -0600

    Add ApiConstants

--- src/main/java/com/trevor/recipe/service/ApiService.java
+++ src/main/java/com/trevor/recipe/service/ApiService.java
@@ -1,9 +1,11 @@
 package com.trevor.recipe.service;
 
+import com.trevor.recipe.model.constants.ApiConstants;
 import com.trevor.recipe.model.recipe.Recipe;
 import com.trevor.recipe.model.response.RecipeResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -11,7 +13,12 @@ import java.util.List;
 public class ApiService {
 
     public List<Recipe> getRandomRecipes() {
-        String uri = "https://api.spoonacular.com/recipes/random?apiKey=e08d347420b548169eb033e623bdfb88&number=10";
+        String uri = UriComponentsBuilder
+                .fromHttpUrl(ApiConstants.BASE_URL)
+                .path("/recipes/random")
+                .queryParam("apiKey", ApiConstants.API_KEY)
+                .queryParam("number", 10)
+                .toUriString();
         RestTemplate restTemplate = new RestTemplate();
         RecipeResponse result = restTemplate.getForObject(uri, RecipeResponse.class);
         if (result == null)
